a0ed8b921c83849ae86e02d7224b34d266900aa5
unknown <finbarr.buttimer@gmail.com>
2026-04-28 17:40:15 +0100

    setting up home page and figuring out api call

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar, IonButton } from '@ionic/angular/standalone';
+import { MyHttpService } from '../services/my-http'
+import { HttpOptions } from '@capacitor/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar, IonButton],
 })
 export class HomePage {
-  constructor() {}
+
+  options: HttpOptions = {
+    url: "https://api.themoviedb.org/3/trending/movie/day?language=en-US",
+  }
+
+  constructor(private mhs:MyHttpService) {}
+
+  ngOnInit() {
+    this.getTrendingMovies();
+  }
+
+  async getTrendingMovies() {
+    var result = await this.mhs.get(this.options);
+    console.log(JSON.stringify(result));
+  }
+
 }
